22202569949f4db219e4904a132ecaf03ce766eb
jvscrpt.r <jvscrptr@gmail.com>
2018-06-03 13:41:00 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import Topbar from './topbar';
 import SearchMobile from './searchmobile';
+import Subscribe from './subscribe';
 import Trendbar from './trendbar';
 import Menu from './menu';
 import Footer from './footer';
@@ -9,12 +10,18 @@ import Footer from './footer';
 class Layout extends Component{
   state = {
     openMenu: false,
+    openSubscribe: false,
     search: '',
     openSearch: false
   }
   toggleMenu = () => {
     this.setState(state => ({
       openMenu: !state.openMenu
+    }))
+  }
+  toggleSubscribe = () => {
+    this.setState(state => ({
+      openSubscribe: !state.openSubscribe
     }))
   }
   handleSearchChange = (e) => {
@@ -34,13 +41,14 @@ class Layout extends Component{
         <title>Techvax.com | Tech How to's, Tips & Tricks and more...</title>
       </Helmet>
       <div style={{ flex: '1 0 auto' }}>
-        <Topbar searchChange={this.handleSearchChange} search={this.state.search} Menu={this.toggleMenu} SearchMobile={this.toggleSearch} />
+        <Topbar searchChange={this.handleSearchChange} search={this.state.search} Menu={this.toggleMenu} Subscribe={this.toggleSubscribe} SearchMobile={this.toggleSearch} />
         <SearchMobile searchChange={this.handleSearchChange} search={this.state.search} open={this.state.openSearch} />
         <Trendbar />
         <Menu open={this.state.openMenu}><span id='close' onClick={this.toggleMenu}>&times;</span></Menu>
+        <Subscribe open={this.state.openSubscribe}><span id='close' onClick={this.toggleSubscribe}>&times;</span></Subscribe>
         {this.props.children}
       </div>
-      <Footer />
+      <Footer Subscribe={this.toggleSubscribe} />
     </div>
     );
   }
