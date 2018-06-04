c076b217143c125a86b8c0b7971439ee9c1bcb6d
jvscrpt.r <jvscrptr@gmail.com>
2018-06-04 07:14:27 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import Topbar from './topbar';
-import SearchMobile from './searchmobile';
-import Subscribe from './subscribe';
+import Searchbar from './search';
 import Trendbar from './trendbar';
 import Menu from './menu';
 import Footer from './footer';
@@ -12,16 +11,11 @@ class Layout extends Component{
     openMenu: false,
     openSubscribe: false,
     search: '',
-    openSearch: false
+    openSearchbar: false
   }
   toggleMenu = () => {
     this.setState(state => ({
       openMenu: !state.openMenu
-    }))
-  }
-  toggleSubscribe = () => {
-    this.setState(state => ({
-      openSubscribe: !state.openSubscribe
     }))
   }
   handleSearchChange = (e) => {
@@ -29,26 +23,26 @@ class Layout extends Component{
       search: e.target.value
     })
   }
-  toggleSearch = () => {
+  toggleSearchbar = () => {
     this.setState(state => ({
-      openSearch: !state.openSearch
+      openSearchbar: !state.openSearchbar
     }))
   }
   render(){
     return(
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Helmet>
-        <title>Techvax.com | Tech How to's, Tips & Tricks and more...</title>
+        <title>Techvax.com | Tech News, How to's, Tips & Tricks and more...</title>
       </Helmet>
       <div style={{ flex: '1 0 auto' }}>
-        <Topbar searchChange={this.handleSearchChange} search={this.state.search} Menu={this.toggleMenu} Subscribe={this.toggleSubscribe} SearchMobile={this.toggleSearch} />
-        <SearchMobile searchChange={this.handleSearchChange} search={this.state.search} open={this.state.openSearch} />
+        <Topbar searchChange={this.handleSearchChange} search={this.state.search} Menu={this.toggleMenu} Searchbar={this.toggleSearchbar} />
+        <Searchbar open={this.state.openSearchbar}><span id='close' onClick={this.toggleSearchbar}>&times;</span></Searchbar>
         <Trendbar />
         <Menu open={this.state.openMenu}><span id='close' onClick={this.toggleMenu}>&times;</span></Menu>
-        <Subscribe open={this.state.openSubscribe}><span id='close' onClick={this.toggleSubscribe}>&times;</span></Subscribe>
         {this.props.children}
+        {this.props.render && this.props.render(this.state.search)}
       </div>
-      <Footer Subscribe={this.toggleSubscribe} />
+      <Footer />
     </div>
     );
   }
