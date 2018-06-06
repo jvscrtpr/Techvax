e439d0b20461f36763403b370027cc8ae679d2a6
jvscrpt.r <jvscrptr@gmail.com>
2018-06-06 10:55:51 -0400

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { MenuStyle } from './styles';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faHome from '@fortawesome/fontawesome-free-solid/faHome';
+import faNewspaper from '@fortawesome/fontawesome-free-solid/faNewspaper';
+import faChessKing from '@fortawesome/fontawesome-free-solid/faChessKing';
+import faLaptop from '@fortawesome/fontawesome-free-solid/faLaptop';
+import faPencilAlt from '@fortawesome/fontawesome-free-solid/faPencilAlt';
+import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag';
 
 class Menu extends Component{
   render(){
@@ -8,12 +15,12 @@ class Menu extends Component{
     <React.Fragment>
       {this.props.open ? <MenuStyle>
         {this.props.children}
-        <Link to='/' onClick={this.props.open}>Home</Link>
-        <Link to='/news' onClick={this.props.open}>News</Link>
-        <Link to='/howto' onClick={this.props.open}>How To's</Link>
-        <Link to='/reviews' onClick={this.props.open}>Reviews</Link>
-        <Link to='/blog' onClick={this.props.open}>Blog</Link>
-        <Link to='/error' onClick={this.props.open}>Shop</Link>
+        <Link to='/' onClick={this.props.open}><FontAwesomeIcon icon={faHome} /> Home</Link>
+        <Link to='/news' onClick={this.props.open}><FontAwesomeIcon icon={faNewspaper} /> News</Link>
+        <Link to='/howto' onClick={this.props.open}><FontAwesomeIcon icon={faChessKing} /> How To's</Link>
+        <Link to='/reviews' onClick={this.props.open}><FontAwesomeIcon icon={faLaptop} /> Reviews</Link>
+        <Link to='/blog' onClick={this.props.open}><FontAwesomeIcon icon={faPencilAlt} /> Blog</Link>
+        <Link to='/error' onClick={this.props.open}><FontAwesomeIcon icon={faShoppingBag} /> Shop</Link>
       </MenuStyle> : null }
     </React.Fragment>
     );
