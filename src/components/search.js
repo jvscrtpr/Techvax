90e6fc241d4d06868c2edab3940233f9b13659d5
Harshit Pant <pantharshit00@gmail.com>
2018-06-06 15:37:08 +0530

    Algolia finalized

--- src/components/search.js
+++ src/components/search.js
@@ -1,29 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { SearchStyle } from './styles';
-import { Link } from 'react-router-dom';
-import { blogcards } from './cards';
+import Link from 'gatsby-link';
+import Card from './card';
+import { InstantSearch } from 'react-instantsearch/dom';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faArrowRight from '@fortawesome/fontawesome-free-solid/faArrowRight';
+import { connectSearchBox, connectHits } from 'react-instantsearch/connectors';
 
-class Searchbar extends Component{
-  render(){
-    return(
-    <React.Fragment>
-      {this.props.open ? <SearchStyle>
-        {this.props.children}
-        <input type='text' placeholder='Search Techvax...' className='search' />
-        <div className='cards'>
-          {blogcards.map(card =>
-            <ul key={card.title}> 
-              <img src={card.img} alt='' className='img' />
-              <Link to={card.href}>{card.title} <FontAwesomeIcon id='arrow' icon={faArrowRight} /></Link>
-              <p>{card.dis}</p>
-              <p id='postusr'>Posted by: {card.usr}</p>
-            </ul>
-          )}
-        </div>
-      </SearchStyle> : null }
-    </React.Fragment>
+const CustomHits = connectHits(({ hits }) => (
+  <div className="cards">
+    {hits.map(card => (
+      <ul key={card.objectID}>
+        <img src={card.frontmatter.image} alt="" className="img" />
+        <a href={card.fields.slug}>
+          {card.frontmatter.title}{' '}
+          <FontAwesomeIcon id="arrow" icon={faArrowRight} />
+        </a>
+        <p>{card.excerpt}</p>
+        {/* Can you also add to artical title inside the <h1 /> JSX element and use id='usr' something like so <h1>{title}<span id='usr'>Posted by: {User}</span></h1> */}
+        <p id="postusr">Will add later</p>
+      </ul>
+    ))}
+  </div>
+));
+
+const MySearchBox = ({ currentRefinement, refine }) => (
+  <input
+    autoFocus
+    autoComplete="off"
+    autoCapitalize="off"
+    autoCorrect="off"
+    type="text"
+    placeholder="Search Techvax..."
+    value={currentRefinement}
+    onChange={e => refine(e.target.value)}
+    type="text"
+    className="search"
+  />
+);
+
+const ConnectedSearchBox = connectSearchBox(MySearchBox);
+
+class Searchbar extends Component {
+  render() {
+    return (
+      <InstantSearch
+        appId={process.env.GATSBY_APP_ID}
+        apiKey={process.env.GATSBY_SEARCH_API_KEY}
+        indexName="blog">
+        <React.Fragment>
+          {this.props.open ? (
+            <SearchStyle>
+              {this.props.children}
+              <ConnectedSearchBox />
+              <CustomHits />
+            </SearchStyle>
+          ) : null}
+        </React.Fragment>
+      </InstantSearch>
     );
   }
 }
