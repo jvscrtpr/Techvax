ac9f924e9db208681c06ed485d4b6154aea06a54
jvscrpt.r <jvscrptr@gmail.com>
2018-06-06 08:07:12 -0400

    Update error.js

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { ErrorStyle } from '../components/styles';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faArrowLeft from '@fortawesome/fontawesome-free-solid/faArrowLeft';
+//import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+//import faArrowLeft from '@fortawesome/fontawesome-free-solid/faArrowLeft';
 
 class Error extends Component{
   render(){
@@ -13,7 +13,8 @@ class Error extends Component{
             <title>404 | Techvax.com</title>
         </Helmet>
         <ErrorStyle>
-            <Link to='/'><FontAwesomeIcon icon={faArrowLeft} /> Back to Techvax.com</Link>
+         {/* mite add this later dont know leve commented out tho
+            <Link to='/'><FontAwesomeIcon icon={faArrowLeft} /> Back to Techvax.com</Link>*/}
             <h1>404</h1>
             <p>We are sorry the page seems to not exist or <br/> we are currently working on it.</p>
         </ErrorStyle>
