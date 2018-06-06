03230058b9751f3fecc9403a8d702db32edc232a
jvscrpt.r <jvscrptr@gmail.com>
2018-06-06 08:00:29 -0400

    Add files via upload

---
+++ src/pages/error.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+import { ErrorStyle } from '../components/styles';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faArrowLeft from '@fortawesome/fontawesome-free-solid/faArrowLeft';
+
+class Error extends Component{
+  render(){
+    return(
+    <div>
+        <Helmet>
+            <title>404 | Techvax.com</title>
+        </Helmet>
+        <ErrorStyle>
+            <Link to='/'><FontAwesomeIcon icon={faArrowLeft} /> Back to Techvax.com</Link>
+            <h1>404</h1>
+            <p>We are sorry the page seems to not exist or <br/> we are currently working on it.</p>
+        </ErrorStyle>
+    </div>
+    );
+  }
+}
+export default Error;
