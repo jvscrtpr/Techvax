6b70ba2a896ae4d4c4719b1333fffbe43eece54e
jvscrpt.r <jvscrptr@gmail.com>
2018-06-06 07:51:30 -0400

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { injectGlobal } from 'styled-components';
-import Navbar from '../components/Navbar';
 import Layout from '../components/layout'
 
 injectGlobal`
