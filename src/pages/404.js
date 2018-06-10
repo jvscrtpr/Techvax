ce3a2fe205ee98eda57d26c60f981e308d0c0fe9
jvscrpt.r <jvscrptr@gmail.com>
2018-06-10 13:14:35 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { ErrorStyle } from '../components/styles';
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Helmet>
+      <title>404 | Techvax.com</title>
+    </Helmet>
+    <ErrorStyle>
+      <h1>404</h1>
+      <p>We are sorry the page seems to not exist or <br/> we are currently working on it.</p>
+    </ErrorStyle>
   </div>
 )
 
