ce1c6b78a00e5715d9b8794fabe70a9733c56f95
Harshit Pant <pantharshit00@gmail.com>
2018-06-12 10:16:18 +0530

    Update BlogPostPreview.js

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,12 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { BlogPostTemplate } from '../../templates/blog-post'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
+const BlogPostPreview = ({ entry, widgetFor, widgetsFor }) => (
   <BlogPostTemplate
     content={widgetFor('body')}
     description={entry.getIn(['data', 'description'])}
     tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
+    author={widgetsFor('author')}
   />
 )
 
